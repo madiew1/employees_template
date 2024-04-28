5346aa71eba74335748bd94f66a1df89601bc83a
Mairbek <madiev.1995@list.ru>
2024-04-28 18:13:30 +0200

    Customization employees list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,17 +7,24 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    { name: 'John C.', salary: 800, increase: false },
+    { name: 'Alex M.', salary: 1000, increase: true },
+    { name: 'Carl S.', salary: 1200, increase: false },
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
